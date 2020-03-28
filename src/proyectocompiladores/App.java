54149efd53f8c2c9acc5f4f19bd574be10ee3b02
Mario Sánchez <Maslz980@outlook.com>
2020-03-28 00:04:12 -0600

    Zoom al grafo

--- src/proyectocompiladores/App.java
+++ src/proyectocompiladores/App.java
@@ -6,6 +6,11 @@
 package proyectocompiladores;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -18,11 +23,19 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java_cup.runtime.Scanner;
 import java_cup.runtime.Symbol;
+import javax.swing.BorderFactory;
 import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.geom.Point2;
+import org.graphstream.ui.geom.Point3;
+import org.graphstream.ui.swingViewer.ViewPanel;
+import org.graphstream.ui.view.Camera;
+import org.graphstream.ui.view.View;
 import org.graphstream.ui.view.Viewer;
 
 /**
@@ -355,7 +368,7 @@ public class App extends javax.swing.JFrame {
                         Node node = graph.getNode(sub1);
                         node.addAttribute("ui.class", "F");
                         node.addAttribute("layout.frozen");
-                        node.addAttribute("x", 128.0);
+                        node.addAttribute("x", 0.0);
                         node.addAttribute("y", 0.0);
 
                         node.addAttribute("ui.style", "fill-color: rgb(255,0,0);"
@@ -368,7 +381,6 @@ public class App extends javax.swing.JFrame {
 
                         //graph.getNode(sub1).addAttribute("ui.style", "fill-mode: none;");
                         //graph.getNode(sub1).addAttribute("ui.style", "text-align: center;");
-
                     }
 
                     //graph.getNode(sub1).addAttribute("ui.style", "text-alignment: center;");
@@ -385,7 +397,7 @@ public class App extends javax.swing.JFrame {
                     sub2 = myTree.graphList.get(i)[1];
 
                     if (sub1.equals("translation_unit") && sub2.equals("")) {
-                        System.out.println("translation_unit");
+                        //System.out.println("translation_unit");
                     } else {
                         graph.addEdge(sub2 + " " + sub1, sub2, sub1);
 
@@ -396,11 +408,36 @@ public class App extends javax.swing.JFrame {
 
                 graph.setAttribute("ui.antialias");
                 graph.setAttribute("ui.quality");
-                Viewer viewer = graph.display();
-                viewer.setCloseFramePolicy(Viewer.CloseFramePolicy.HIDE_ONLY);
+
                 //viewer.disableAutoLayout();
                 //graph.display().disableAutoLayout();
                 //myTree.printGraph();
+                /*
+                Viewer viewer = graph.display();
+
+                viewer.setCloseFramePolicy(Viewer.CloseFramePolicy.HIDE_ONLY);
+                 */
+                JFrame frame = new JFrame("GRAPH");
+                @SuppressWarnings("serial")
+                JPanel panel = new JPanel(new GridLayout()) {
+                    @Override
+                    public Dimension getPreferredSize() {
+                        return new Dimension(640, 480);
+                    }
+                };
+                panel.setBorder(BorderFactory.createLineBorder(Color.blue, 5));
+                Viewer viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_GUI_THREAD);
+                ViewPanel viewPanel = viewer.addDefaultView(false);
+                viewer.enableAutoLayout();
+                viewPanel.getCamera().setViewPercent(0.25);
+                viewPanel.getCamera().setViewCenter(0, 0, 0);
+                viewPanel.requestFocusInWindow();
+                panel.add(viewPanel);
+                frame.add(panel);
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+
             } catch (FileNotFoundException ex) {
 
                 Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
--- src/proyectocompiladores/TreeNode.java
+++ src/proyectocompiladores/TreeNode.java
@@ -129,7 +129,7 @@ public class TreeNode {
         Values temp;
 
         myParent = mySubParent;
-        System.out.println("this is the parent child: " + myParent);
+        //System.out.println("this is the parent child: " + myParent);
 
         /*graphList = new ArrayList<String[]>();
         System.out.println("Parameter List: " + graphList.toString());
@@ -154,7 +154,7 @@ public class TreeNode {
           
             StringBuilder name = new StringBuilder(myParent);
 
-            System.out.println("This is child's name: " + child.getValue().value.toString());
+            //System.out.println("This is child's name: " + child.getValue().value.toString());
 
             name.append(child.getValue().value.toString() + Integer.toString(index));
             //String name = myParent + child.getValue().value.toString() + Integer.toString(index);
